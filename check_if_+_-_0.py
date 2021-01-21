cacb6d93e8190c2d9f92e9f8cda844021716c37b
ZainabFatima507 <76115712+ZainabFatima507@users.noreply.github.com>
2021-01-21 17:37:35 +0530

    Add files via upload

---
+++ check_if_+_-_0.py
@@ -0,0 +1,10 @@
+num = input ( "type a number:")
+
+if num >= "0":
+    if num > "0":
+        print ("the number is positive.")
+    else:
+        print("the number is zero.")
+else:
+    print ("the number is negative.")
+
